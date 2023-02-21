dd4b9307b534e9ef5ae9c24ac54b08d868c7b8a1
diego-vecch <josediegoferia@gmail.com>
2023-02-20 21:18:03 -0300

    feat: test first integration fetch

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,36 @@
-import {Button, Text, View} from 'react-native'
-import ButtonApp from '../components/ButtonApp'
+import { View, Text } from "react-native";
+import ButtonApp from "../components/ButtonApp";
+import { useState, useEffect } from "react";
 
-export default function HomePage({navigation}) {
-    return(
-        <View className="bg-indigo-300 h-full flex items-center">
-            <ButtonApp text='Login'
-            onPress={()=>{navigation.navigate('Login')}}/>
-        </View>
-    )
-    
+export default function HomePage({ navigation }) {
+  const [username, setUsername] = useState(null);
+
+  useEffect(function () {
+    async function getDataUsers() {
+      try {
+        let rta = await fetch("http://127.0.0.1:8000", {
+          method: "GET",
+        });
+        let resJson = await rta.json();
+        setUsername(resJson.groups);
+        
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getDataUsers();
+  },[]);
+  
+  return (
+    <View className="bg-indigo-300 h-full flex items-center">
+      <ButtonApp
+        text="Login"
+        onPress={() => {
+          navigation.navigate("Login");
+        }}
+      />
+      <Text>Hola estoy </Text>
+      <Text> haciendo fetch en: {username}</Text>
+    </View>
+  );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,11 @@
-import {Text} from 'react-native'
-export default function Profile({navigation}) {
-    return(
-        <Text className="bg-indigo-300 text-violet-700 h-full text-center text-3xl">Welcome</Text>
-    )
+import { Text, View } from "react-native";
+
+export default function Profile() {
+  return (
+    <View className="bg-indigo-300 text-violet-700 h-full items-center">
+      <Text className="bg-indigo-300 text-violet-700 h-20 text-center text-3xl">
+        Welcome
+      </Text>
+    </View>
+  );
 }
